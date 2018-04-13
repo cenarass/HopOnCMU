09d6ea3dcd6783a5d0fc4652cf3461739a34e21b
naotedigo <pedromramaral@hotmail.com>
2018-04-13 20:43:06 +0100

    Acrestei os lyfecicle em todas as activities e coloquei intents nos botoes que nao precisam que confirmacao do servidor, tipo sigin button ---> sign in activity

--- app/src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/LoginActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/LoginActivity.java
@@ -1,7 +1,9 @@
 package pt.ulisboa.tecnico.cmov.hoponcmu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +11,41 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void startSignUpActivity(View v) {
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
     }
 
 
--- app/src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/hoponcmu/MainActivity.java
@@ -1,7 +1,9 @@
 package pt.ulisboa.tecnico.cmov.hoponcmu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void startMonumentActivity(View v) {
+        Intent intent = new Intent(MainActivity.this, MonumentActivity.class);
+        startActivity(intent);
+    }
+
+    public void startScoreActivity(View v) {
+        Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+        startActivity(intent);
+    }
+
+
 }
